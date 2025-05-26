7f9ea35bb8a6ed936b81f7e02c08908f2e06e58c
DEBUG42 <2964568505@qq.com>
2025-05-26 18:33:23 +0800

    究极改bug

--- Physics/CollisionDetector.hpp
+++ Physics/CollisionDetector.hpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <../Core/Vehicle.hpp>
 #include <cmath>
+#include <Task.hpp>
 //三辆车的运动控制
 
 
@@ -15,6 +16,51 @@ public:
 	//前车与后车相对距离
 	float distance;
 	float epsilon; // 防止浮点数误差
+/*
+p
+/*
+pi=3.14159265358979323846
+18 8000
+17 11000
+16 14000
+15 26000
+14 29000
+13 32000
+12 40000+2500pi+3800=43800+7500pi=   51635.981634
+11 40000+2500pi+6200=46200+7500pi=   54035.981634
+10 40000+2500pi+9800=49800+7500pi=   57635.981634
+9 40000+2500pi+12200=52200+7500pi=   60035.981634
+8 40000+2500pi+15800=55800+7500pi=   63635.981634
+7 40000+2500pi+18200=58200+7500pi=   66035.981634
+6 40000+2500pi+21800=61800+7500pi=   69635.981634
+5 40000+2500pi+24200=64200+7500pi=   72035.981634
+4 40000+2500pi+27800=67800+7500pi=   75635.981634
+3 40000+2500pi+30200=70200+7500pi=   78035.981634
+2 40000+2500pi+33800=73800+7500pi=   81635.981634
+1 40000+2500pi+36200=76200+7500pi=   84035.981634
+*/
+	int  towards_device=0;
+	float device_position[19]={
+		-1000.0f,
+		84.035981634f,
+		81.635981634f,
+		78.035981634f,
+		75.635981634f,
+		72.035981634f,
+		69.635981634f,
+		66.035981634f,
+		63.635981634f,
+		60.035981634f,
+		57.635981634f,
+		54.035981634f,
+		51.635981634f,
+		32.000f,
+		29.000f,
+		26.000f,
+		14.000f,
+		11.000f,
+		8.000f;
+	};
 
 public:
 
@@ -24,7 +70,7 @@ public:
      */
     VehicleStateMachine(Vehicle* v) : vehicle(v) {}
 
-    void update(float deltaTime, Vehicle* leadingVehicle) {
+    void update(float Timescale,float deltaTime, Vehicle* leadingVehicle) {
 	//当前主小车相对原点位置
 	VehiclePosition =std::fmod(vehicle->m_state.position,95.671963268f);
 	//leadingVehicle相对原点位置
@@ -40,7 +86,7 @@ public:
 	vehicle->m_state.operationTimer=0.0f;
 	
 //到车库停车的判断和处理
-	if(vehicle->m_state.motionState == Vehicle::MotionState::Stopped && abs(VehiclePosition - 车库位置)<epsilon){
+	if(vehicle->m_state.motionState == Vehicle::MotionState::Stopped && fabs(VehiclePosition - device_position[towards_device])<epsilon){
 		vehicle->m_state.operationTimer+=deltaTime;
 		
 		if (vehicle->m_state.operationTimer >= vehicle->m_loadTime) {
@@ -54,8 +100,8 @@ public:
             vehicle->m_state.motionState = Vehicle::MotionState::Decelerating;
         }
 //到车库提前减速	
-	else if((vehicle->m_state.currentSpeed)*(vehicle->m_state.currentSpeed)/(2*vehicle->m_acceleration)<= (abs(目标车库的位置-VehiclePosition))){
-			vehicle.motionState = Vehicle::MotionState::Decelerating;
+	else if((vehicle->m_state.currentSpeed)*(vehicle->m_state.currentSpeed)/(2*vehicle->m_acceleration)<= ((device_position[towards_device]-VehiclePosition))){
+			vehicle->m_state.motionState = Vehicle::MotionState::Decelerating;
 		}
 //弯道减速	
 	//处理下面那个弯道的减速
@@ -79,13 +125,13 @@ public:
 	switch (vehicle->m_state.motionState) {
             case Vehicle::MotionState::Accelerating:
 			//判断上一辆车的距离
-			accelerate(deltaTime);
+			accelerate(Timescale,deltaTime);
 			break;
             case Vehicle::MotionState::Decelerating:
-                decelerate(deltaTime);
-                if (vehicle->currentSpeed <= 0.0f) {
-                    vehicle->currentSpeed = 0.0f;
-                    vehicle->motionState = Vehicle::MotionState::Stopped;
+                decelerate(Timescale,deltaTime);
+                if (vehicle->m_state.currentSpeed <= 0.0f) {
+                    vehicle->m_state.currentSpeed = 0.0f;
+                    vehicle->m_state.motionState= Vehicle::MotionState::Stopped;
                 }
             break;
             case Vehicle::MotionState::Cruising:
@@ -97,11 +143,11 @@ public:
                 break;
         }
 //位置的更新
-	vehicle->m_state.position += vehicle->m_state.currentSpeed * deltaTime;
+	vehicle->m_state.position += vehicle->m_state.currentSpeed *Timescale* deltaTime;
     }
 private:
-    void accelerate(float deltaTime) {
-        vehicle->m_state.currentSpeed += vehicle.m_acceleration * deltaTime;
+    void accelerate(float Timescale,float deltaTime) {
+        vehicle->m_state.currentSpeed += vehicle->m_acceleration    *Timescale * deltaTime;
 //直线上且超过最大速度
 		if((VehiclePosition>=0.f)&&(VehiclePosition<=40.0f)&&(vehicle->m_state.currentSpeed > vehicle->m_maxStraightSpeed)){
             vehicle->m_state.currentSpeed = vehicle->m_maxStraightSpeed;
@@ -118,8 +164,8 @@ private:
 		}
     }
 
-    void decelerate(float deltaTime) {
-        vehicle->m_state.currentSpeed -= vehicle->m_acceleration * deltaTime;
+    void decelerate(float Timescale,float deltaTime) {
+        vehicle->m_state.currentSpeed -= vehicle->m_acceleration  *Timescale  *Timescale * deltaTime;
         if (vehicle->m_state.currentSpeed < 0.0f) {
             vehicle->m_state.currentSpeed= 0.0f;
         }
@@ -137,7 +183,6 @@ Vehicle myvehicle[3]={
 };
 VehicleStateMachine vsm1,vsm2,vsm3;
 MotionController3() {
-
 	vsm1 = VehicleStateMachine(&myvehicle[0]);
 	vsm2 = VehicleStateMachine(&myvehicle[1]);
 	vsm3 = VehicleStateMachine(&myvehicle[2]);
